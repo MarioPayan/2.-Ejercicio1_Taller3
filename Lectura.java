ca2bcd1569055644fbf8257dbbc88c92d992d7e1
Juan Sebastian <juan.sebastian.rivas@correounivalle.edu.co>
2015-04-11 15:54:30 -0500

    Actualizacion de la clase Lectura

---
+++ Lectura.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.util.*;
+
+public class Lectura 
+{
+    ArrayList circunferencia, coordenadas;
+    int [][] puntos;
+    
+    public void setCircunferencia(ArrayList Circunferencia)
+    {
+        this.circunferencia = Circunferencia;
+    }
+    
+    public ArrayList getCircunferencia()
+    {
+        return circunferencia;
+    }
+    
+    public void setCoordenadas(ArrayList Coordenadas)
+    {
+        this.coordenadas = Coordenadas;
+    }
+    
+    public ArrayList getCoordenadas()
+    {
+        return coordenadas;
+    }
+    
+    public void setPuntos(int [][] puntos)
+    {
+        this.puntos = puntos;
+    }
+    
+    public int [][] getPuntos()
+    {
+        return puntos;
+    }
+    
+    public void leer (String path)
+    {
+        File archivo;
+        FileReader fileReader;
+        BufferedReader bufferedReader;
+        String linea;
+    
+        try
+        {
+            archivo = new File(path);
+            fileReader = new FileReader (archivo);
+            bufferedReader = new BufferedReader(fileReader);
+            circunferencia = new ArrayList();
+            coordenadas = new ArrayList();
+            
+            for(int i = 0; (linea = bufferedReader.readLine()) != null; i++)
+            {
+                if (i == 0) 
+                {
+                    circunferencia.add(linea);
+                }
+                
+                else 
+                {
+                    coordenadas.add(linea);
+                }
+            }
+        }
+        
+        catch(FileNotFoundException fnfe)
+        {
+            System.err.println(fnfe.getMessage());
+        }
+        
+        catch(IOException ioe)
+        {
+            System.err.println(ioe.getMessage());
+        }
+        
+        catch(Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void Tokenizer (ArrayList lista)
+    {
+        String lineas, valor;
+        StringTokenizer token;
+        ArrayList cloneCircunferencia, cloneCoordenadas;
+        
+        if (lista.size() == 1) 
+        {
+            lineas = (String) lista.get(0);
+            token = new StringTokenizer(lineas, " ,;()");
+            cloneCircunferencia = new ArrayList();
+            
+            while(token.hasMoreTokens())
+            {
+                valor = (String) token.nextElement();
+                cloneCircunferencia.add(valor);
+            }
+            
+            setCircunferencia(cloneCircunferencia);
+        }
+        
+        else 
+        {   
+            cloneCoordenadas = new ArrayList();
+            
+            for (int j = 0; j < lista.size(); j++)
+            {
+                lineas = (String) lista.get(j);
+                token = new StringTokenizer(lineas, " ,()");
+                
+                while(token.hasMoreTokens())
+                {
+                    valor = (String) token.nextElement();
+                    cloneCoordenadas.add(valor);
+                }
+            }
+            
+            setCoordenadas(cloneCoordenadas);
+        }
+    }
+    
+    public void Puntos (ArrayList cloneCoordenadas) 
+    {
+        int puntoX, puntoY, posicion;
+        int [][] clonePuntos;
+        
+        clonePuntos = new int [cloneCoordenadas.size()/2][2];
+        posicion = 0;
+        
+        for (int x = 0; x < (cloneCoordenadas.size() - 1); x += 2) 
+        {
+            puntoX = Integer.parseInt((String) cloneCoordenadas.get(x));
+            puntoY = Integer.parseInt((String) cloneCoordenadas.get(x + 1));
+            
+            clonePuntos [posicion][0] = puntoX;
+            clonePuntos [posicion][1] = puntoY;
+            
+            posicion++;
+        }
+        
+        setPuntos(clonePuntos);
+    }
+}
